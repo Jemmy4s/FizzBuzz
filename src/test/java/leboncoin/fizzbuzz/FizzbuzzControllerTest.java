75c7dad69c39d2ca4515ba093cb842026eeeaca1
Valentin V <jemmy4s57@gmail.com>
2022-02-17 15:36:45 +0100

    (fix): Boilplate error controller

--- src/test/java/leboncoin/fizzbuzz/FizzbuzzControllerTest.java
+++ src/test/java/leboncoin/fizzbuzz/FizzbuzzControllerTest.java
@@ -1,19 +1,14 @@
 package leboncoin.fizzbuzz;
 
 import com.leboncoin.fizzbuzz.controller.FizzbuzzController;
-import com.leboncoin.fizzbuzz.controller.FizzbuzzControllerImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
 
 
 @SpringBootTest
 public class FizzbuzzControllerTest {
-
 
     @Autowired
     FizzbuzzController fbcontroller;
